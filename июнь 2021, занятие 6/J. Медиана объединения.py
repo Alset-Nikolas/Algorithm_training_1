73f734e69d800f8a28bb6b663f9f0975f6a59d3b
Alset.Nikolas@yandex.ru <Alset.Nikolas@yandex.ru>
2022-03-18 10:13:47 +0300

    Закончил делать в 6 дз (J. Медиана объединения)

---
+++ июнь 2021, занятие 6/J. Медиана объединения.py
@@ -0,0 +1,32 @@
+def calc_item(k, L, R):
+    i_L = 0
+    i_R = 0
+    res_i = 0
+    res = None
+    while res_i != k:
+        if i_L < len(L) and i_R < len(R):
+            if L[i_L] > R[i_R]:
+                res = R[i_R]
+                i_R += 1
+            else:
+                res = L[i_L]
+                i_L += 1
+        elif i_L == len(L):
+            res = R[i_R]
+            i_R += 1
+        elif i_R == len(R):
+            res = L[i_L]
+            i_L += 1
+        res_i += 1
+    return res
+
+
+N, K = list(map(int, input().split()))
+Lists = []
+for i in range(N):
+    Lists.append(list(map(int, input().split())))
+for i in range(N):
+    first_list = Lists[i]
+    for j in range(i+1, N):
+        second_list = Lists[j]
+        print(calc_item(K, first_list, second_list))
